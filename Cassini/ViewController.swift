c9d69fbff8e564d32a6e809e6189df322f73e40d
hernan <hquintana@devosinc.com>
2016-01-03 20:55:30 -0500

    commit inicial

---
+++ Cassini/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Hernan Quintana on 25/12/15.
+//  Copyright © 2015 Jsatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController{
+            if let identifier = segue.identifier{
+                switch identifier{
+                    case "Earth":
+                        ivc.imageURL = DemoURL.NASA.Earth
+                        ivc.title = "Earth"
+                    case "Cassini":
+                        ivc.imageURL = DemoURL.NASA.Cassini
+                        ivc.title = "Cassini"
+                    case "Saturn":
+                        ivc.imageURL = DemoURL.NASA.Saturn
+                        ivc.title = "Saturn"
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+}
+
